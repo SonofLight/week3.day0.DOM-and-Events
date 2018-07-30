153c3b983b91ad3a6226014013f53e25e35ce9c0
Corey Gumbs <cgumbs@UNKNOWN-172-133-37-X.yahoo.com>
2018-07-30 09:20:44 -0400

    [chore] update code

--- addingStyle/lib/js/main.js
+++ addingStyle/lib/js/main.js
@@ -3,5 +3,8 @@
 document.querySelector('.right_side').style.background="#d3c26f";
 
 
-document.querySelector('.name').setAttribute("style", "color:yellow; text-shadow: -6px 5px 2px;");
+let nameDiv =  document.querySelectorAll('.name')
 
+for(let i = 0; i < nameDiv.length; i++ ){
+	console.log(nameDiv[i].setAttribute("style", "color:yellow; text-shadow: -6px 5px 2px;"));
+}
